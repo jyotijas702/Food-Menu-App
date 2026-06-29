fa09b36eb1dc3c87aa4e3bbf7df8b62a1c34c0a0
Jyoti Kumari <1996.jyotisuman@gmail.com>
2026-06-29 19:50:17 +0530

    added navbar and create page

--- src/routes/food-items.ts
+++ src/routes/food-items.ts
@@ -6,4 +6,8 @@ router.get("/", async (req: Request, res: Response) => {
   res.render("food-items/index");
 });
 
+router.get("/create", async (req: Request, res: Response) => {
+  res.render("food-items/create");
+});
+
 export default router;
