e23a4492ef716e349fc9aa8e52b4ebf0ce7d6829
Sameer Kumar <kumar.sam1612@gmail.com>
2026-06-30 14:24:36 +0530

    added food items card layout

--- src/routes/food-items.ts
+++ src/routes/food-items.ts
@@ -1,10 +1,52 @@
-import { Router, Request, Response } from "express";
+import { Request, Response, Router } from "express";
 import { type FoodItem } from "../types/food-item";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   const itemList: Array<FoodItem> = [
+    {
+      ratings: 5,
+      name: "Mysore Masala Dosa",
+      imageUrl:
+        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
+      price: 100,
+    },
+    {
+      ratings: 3,
+      name: "Dosa",
+      imageUrl:
+        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
+      price: 100,
+    },
+    {
+      ratings: 4,
+      name: "Dosa",
+      imageUrl:
+        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
+      price: 100,
+    },
+    {
+      ratings: 5,
+      name: "Dosa",
+      imageUrl:
+        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
+      price: 100,
+    },
+    {
+      ratings: 5,
+      name: "Dosa",
+      imageUrl:
+        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
+      price: 100,
+    },
+    {
+      ratings: 5,
+      name: "Dosa",
+      imageUrl:
+        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
+      price: 100,
+    },
     {
       ratings: 5,
       name: "Dosa",
