972e70b5e6af20a620a3e9e0d56c01e2808c612e
Jyoti Kumari <1996.jyotisuman@gmail.com>
2026-07-02 13:34:16 +0530

    temprory commit

--- src/routes/food-items.ts
+++ src/routes/food-items.ts
@@ -1,60 +1,14 @@
 import { Request, Response, Router } from "express";
-import { type FoodItem } from "../types/food-item";
+// import { type FoodItem } from "../types/food-item";
+import FoodItemModel from "../models/food-item";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  const itemList: Array<FoodItem> = [
-    {
-      ratings: 5,
-      name: "Mysore Masala Dosa",
-      imageUrl:
-        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
-      price: 100,
-    },
-    {
-      ratings: 3,
-      name: "Dosa",
-      imageUrl:
-        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
-      price: 100,
-    },
-    {
-      ratings: 4,
-      name: "Dosa",
-      imageUrl:
-        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
-      price: 100,
-    },
-    {
-      ratings: 5,
-      name: "Dosa",
-      imageUrl:
-        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
-      price: 100,
-    },
-    {
-      ratings: 5,
-      name: "Dosa",
-      imageUrl:
-        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
-      price: 100,
-    },
-    {
-      ratings: 5,
-      name: "Dosa",
-      imageUrl:
-        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
-      price: 100,
-    },
-    {
-      ratings: 5,
-      name: "Dosa",
-      imageUrl:
-        "https://pipingpotcurry.com/wp-content/uploads/2020/12/how-to-make-dosa-crispy-PipingPotCurry.jpg",
-      price: 100,
-    },
-  ];
+    const itemList = await FoodItemModel.find()
+      console.log("Items from MongoDB:", itemList);
+
+  
   res.render("food-items/index", { items: itemList });
 });
 
@@ -62,4 +16,17 @@ router.get("/create", async (req: Request, res: Response) => {
   res.render("food-items/create");
 });
 
+
+router.post("/", async (req: Request, res: Response) => {
+    const { name, price, ratings, imageUrl } = req.body;
+
+    try {
+        const newItem = await FoodItemModel.create({ name, price, ratings, imageUrl });
+        console.log("save", newItem)
+        res.redirect("/food-items");
+    } catch (error) {
+        console.error("Database Save Error:", error);
+        res.status(500).send("Server Error");
+    }
+});
 export default router;
